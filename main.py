2ae8dc12e8050257bdc2995f54c48aa8fe6a59fd
Lorenz Saalmann <l.s.spirit@icloud.com>
2025-01-01 17:10:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ stages = [Stage(20, 200, 3500, 1500, 0),
           Stage(5, 50, 3500, 150, 10)
           ]
 
-stages = optimize_staging_two_stages(stages, 1/11, 11.1, 1000)
+stages = optimize_staging_two_stages(stages, 1/11, 10, 1000)
 
 #gravity_turn = flight_sim.GravityTurn(250, 3000, 3.71)
-gravity_turn = flight_sim.GravityTurn(1000, 8500, 8.395)
+gravity_turn = flight_sim.GravityTurn(1000, 9250, 8.4)
 sim = FlightSim(stages, gravity_turn, 270000)
 result = sim.simulate()
 
